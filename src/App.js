ccd0d52aa1d7e1457ed9c1197f7fc8dddad667c0
AlifSakib <alifsakib@gmail.com>
2022-10-10 22:13:08 +0600

    router setup done for Root component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
+import { RouterProvider } from "react-router-dom";
 import "./App.css";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Home from "./components/Home";
+import { router } from "./Routes/routes";
 
 function App() {
   return (
     <div className="App font-quickSand">
-      <Header></Header>
-      <Home></Home>
-      <Footer></Footer>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
